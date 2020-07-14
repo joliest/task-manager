7b923c75070b53f40b01f792b7156def0470f05a
Joliver Estampador <joliverestampador@gmail.com>
2020-07-14 19:30:10 +0800

    Basic mongoose

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -5,3 +5,43 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
     useCreateIndex: true
 })
+
+const User = mongoose.model('User', {
+    name: {
+        type: String
+    },
+    age: {
+        type: Number
+    }
+})
+
+const Task = mongoose.model('Task', {
+    description: {
+        type: String
+    },
+    completed: {
+        type: Boolean
+    }
+})
+
+const task = new Task({
+    description: 'My first task',
+    completed: true
+})
+
+task.save().then(res => {
+    console.log(res)
+}).catch(err => {
+    console.log(err)
+})
+
+const me = new User({ 
+    name: 'Joli',
+    age: 23
+})
+
+me.save().then(res => {
+        console.log('Me: ', res)
+    }).catch(err => {
+        console.log(err)
+    });
