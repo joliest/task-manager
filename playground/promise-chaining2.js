8c8fdc3b6378a0dbfb8bd446967a0ba417470de2
Joliver Estampador <joliver.estampador@tooltwist.com>
2020-07-17 14:48:39 +0800

    Converting promise chaining into async and await

--- playground/promise-chaining2.js
+++ playground/promise-chaining2.js
@@ -1,14 +1,24 @@
 require('../src/db/mongoose')
 const Task = require('../src/models/task')
 
-Task.findByIdAndDelete('5f1110b165b8625fb74cb2db').then(task => {
-  console.log(task)
-  return Task.countDocuments({ completed: false })
-}).then(numberOfIncompleteTasks => {
-  console.log('Incomplete Task No: ', numberOfIncompleteTasks)
-}).catch(e => {
-  console.log(e)
+// Task.findByIdAndDelete('5f1110b165b8625fb74cb2db').then(task => {
+//   console.log(task)
+//   return Task.countDocuments({ completed: false })
+// }).then(numberOfIncompleteTasks => {
+//   console.log('Incomplete Task No: ', numberOfIncompleteTasks)
+// }).catch(e => {
+//   console.log(e)
+// })
+
+const deleteTaskAndCount = async (id) => {
+  const userToDelete = Task.findOneAndDelete({ id })
+  const count = Task.countDocuments({ completed: false })
+
+  return count;
+}
+
+deleteTaskAndCount('5f1110b165b8625fb74cb2db').then(result => {
+  console.log(result)
+}).catch(error => {
+  console.log(error)
 })
-
-
-// 5f1110b165b8625fb74cb2db
