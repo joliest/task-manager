f1b305e538fd6cf8938f776323857bc5f972f734
Joliver Estampador <joliver.estampador@tooltwist.com>
2020-07-17 10:47:12 +0800

    Completed Task model

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express =  require('express')
 require('./db/mongoose')
 const User = require('./models/user')
+const Task = require('./models/task')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -12,6 +13,16 @@ app.post('/users', (req, res) => {
     const user = new User(req.body)
 
     user.save().then(result => {
+        res.send(result)
+    }).catch(error => {
+        res.status(400).send(error)
+    })
+})
+
+app.post('/tasks', (req, res) => {
+    const task = new Task(req.body)
+
+    task.save().then(result => {
         res.send(result)
     }).catch(error => {
         res.status(400).send(error)
