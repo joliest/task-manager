12a01a4dac1237e0d51b742cdfd6e7f660e04341
Joliver Estampador <joliver.estampador@tooltwist.com>
2020-07-17 13:01:13 +0800

    Reading users

--- src/index.js
+++ src/index.js
@@ -19,6 +19,26 @@ app.post('/users', (req, res) => {
     })
 })
 
+app.get('/users', (req, res) => {
+    User.find({ }).then(users => {
+        res.send(users)
+    }).catch(() => {
+        res.status(500).send()
+    })
+})
+
+app.get('/users/:id', (req, res) => {
+    User.findById({ _id: req.params.id }).then(user => {
+        if (!user) {
+            return res.status(404).send()
+        }
+        
+        res.send(user)
+    }).catch(() => {
+        res.status(500).send()
+    })
+})
+
 app.post('/tasks', (req, res) => {
     const task = new Task(req.body)
 
