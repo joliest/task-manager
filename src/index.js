57eb810c493ad2ff8cd53a827d211e542a355ab2
Joliver Estampador <joliverestampador@gmail.com>
2020-07-18 21:52:51 +0800

    Applying findByIdAndUpdate() to tasks

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express =  require('express')
 require('./db/mongoose')
 const User = require('./models/user')
 const Task = require('./models/task')
+const { ObjectID } = require('mongodb')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -74,7 +75,7 @@ app.post('/tasks', async (req, res) => {
         task.save()
         res.status(201).send(result)
     } catch (e) {
-        res.status(400).send(error)
+        res.status(400).send(e)
     }
 })
 
@@ -82,6 +83,31 @@ app.get('/tasks', async (req, res) => {
     try {
         const tasks = await Task.find({})
         res.send(tasks)
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+app.patch('/tasks/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['description', 'completed']
+    const isUpdateValid = updates.every(update => allowedUpdates.includes(update))
+
+    if (!isUpdateValid) {
+        return res.status(400).send({ error: 'Invalid updates' })
+    }
+
+    try {
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { 
+            new: true, 
+            runValidators: true
+        })
+
+        if (!task) {
+            return res.status(404).send()
+        }
+
+        res.send(task)
     } catch (e) {
         res.status(500).send()
     }
