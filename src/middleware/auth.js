d928a6d71e1bd0aa5a955fc8f45548295ddcd12b
Joliver Estampador <joliverestampador@gmail.com>
2020-07-23 20:46:55 +0800

    Applying express middle ware to authenticate request

---
+++ src/middleware/auth.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken')
+const User = require('../models/user')
+
+const auth = async (req, res, next) => {
+    try {
+        const token = req.header('Authorization').replace('Bearer ', '')
+
+        // decodes the binded data e.g. _id
+        const decoded = jwt.verify(token, 'thisIsASecretToken')
+
+        // find user with the id and if it has the given token
+        const user = await User.findOne({ _id: decoded._id, 'tokens.token': token })
+
+        if(!user) {
+            throw new Error()
+        }
+
+        req.user = user
+        
+        next()
+    } catch (e) {
+        res.status(401).send({error: 'Please authenticate'})
+    }
+}
+
+module.exports = auth
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const auth = require('../middleware/auth')
 const User = require('../models/user')
 
 const router = new express.Router()
@@ -26,13 +27,10 @@ router.post('/users/login', async (req, res) => {
     }
 })
 
-router.get('/users', async (req, res) => {
-    try {
-        const users = await User.find({ });
-        res.send(users)
-    } catch (e) {
-        res.status(500).send()
-    }
+
+// profile of authenticated user
+router.get('/users/me', auth, async (req, res) => {
+    res.send(req.user)
 })
 
 router.get('/users/:id', async (req, res) => {
