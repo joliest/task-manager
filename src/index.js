a9cf423805543defe7bfcbe4e9414cccd73fe5bd
Joliver Estampador <joliverestampador@gmail.com>
2020-07-23 20:05:30 +0800

    cleanup

--- src/index.js
+++ src/index.js
@@ -7,12 +7,6 @@ const TaskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
-// make sure it's above app.use() calls
-// middleware
-app.use((req, res, next) => {
-    res.status(503).send('Service temporarily unavailable')
-})
-
 app.use(express.json())
 app.use(UserRouter)
 app.use(TaskRouter)
@@ -20,23 +14,3 @@ app.use(TaskRouter)
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
-
-// const jwt = require('jsonwebtoken')
-
-// const myFunction = async () => {
-//     // returns new token
-//     const token = jwt.sign({ 
-//         // data embedded to your token
-//         _id: 'abcd123'
-//         // add secret code
-//      }, 'thisIsASecret', {
-//          expiresIn: '7 days'
-//      });
-//      console.log(token)
-
-//      // to verify if its a valid token, secret sould be the same
-//      const data = jwt.verify(token, 'thisIsASecret')
-//      console.log(data)
-// }
-
-// myFunction()
