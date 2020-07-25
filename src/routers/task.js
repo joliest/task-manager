fd3c82e9f7527052ffac7b61417d57e9453cee14
Joliver Estampador <joliverestampador@gmail.com>
2020-07-25 16:00:58 +0800

    VIewing task by id only by currenly logged in user

--- src/routers/task.js
+++ src/routers/task.js
@@ -51,12 +51,16 @@ router.patch('/tasks/:id', async (req, res) => {
     }
 })
 
-router.get('/tasks/:id', async (req, res) => {
+router.get('/tasks/:id', auth, async (req, res) => {
+    const _id = req.params.id
+
     try {
-        const task = await Task.findById({ _id: req.params.id })
+        const task = await Task.findOne({ _id, owner: req.user._id })
+
         if (!task) {
             return res.status(404).send()
         }
+        
         res.send(task)
     } catch (e) {
         res.status(500).send()
