e35d7d31064b9f708f3cf2ab490deaf1b29610ec
Joliver Estampador <joliverestampador@gmail.com>
2020-07-25 14:32:48 +0800

    manual way

--- src/models/user.js
+++ src/models/user.js
@@ -80,6 +80,17 @@ userSchema.methods.generateAuthToken = async function () {
     return token
 }
 
+userSchema.methods.getPublicProfile = function () {
+    const user = this
+
+    const userObject = user.toObject()
+
+    delete userObject.password
+    delete userObject.tokens
+
+    return userObject
+}
+
 // hash the plain text before saving
 userSchema.pre('save', async function(next) {
 
