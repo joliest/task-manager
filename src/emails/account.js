1b55aa212c7ca73f7c18f56150bc9d261f9ac3ce
Joliver Estampador <joliverestampador@gmail.com>
2020-08-02 17:41:02 +0800

    Adding Welcome and Cancellation Email

--- src/emails/account.js
+++ src/emails/account.js
@@ -4,3 +4,25 @@ const sgMail = require('@sendgrid/mail')
 
 sgMail.setApiKey(sendgridApiKey)
 
+const sendWelcomeEmail = (email, name) => {
+    sgMail.send({
+        to: email,
+        from: 'joliverestampador@gmail.com',
+        subject: 'Thanks for joining in!',
+        text: `Welcome to the app, ${name}. Let me know how you get along with the app.`
+    })
+}
+
+const sendCancellationEmail = (email, name) => {
+    sgMail.send({
+        to: email,
+        from: 'joliverestampador@gmail.com',
+        subject: 'I\'m sorry for cancelling your account',
+        text: 'Thank you for being part of the team'
+    })
+}
+
+module.exports = {
+    sendWelcomeEmail,
+    sendCancellationEmail
+}
--- src/routers/user.js
+++ src/routers/user.js
@@ -3,6 +3,7 @@ const auth = require('../middleware/auth')
 const multer = require('multer')
 const sharp = require('sharp')
 const User = require('../models/user')
+const { sendWelcomeEmail, sendCancellationEmail } = require('../emails/account')
 
 const router = new express.Router()
 const upload = multer({
@@ -23,6 +24,7 @@ router.post('/users', async (req, res) => {
 
     try {
         await user.save();
+        sendWelcomeEmail(user.email, user.name)
         const token = await user.generateAuthToken()
         res.status(201).send({user, token})
     } catch(e) {
@@ -116,6 +118,7 @@ router.delete('/users/me', auth, async (req, res) => {
 
         // deleting yourself
         await req.user.remove()
+        sendCancellationEmail(req.user.email, req.user.name)
 
         res.send(req.user)
     } catch (e) {
