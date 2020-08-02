3408bf70c7e05c204cee7a9095b9b20827d4f315
Joliver Estampador <joliverestampador@gmail.com>
2020-08-02 13:26:29 +0800

    cleanup

--- src/index.js
+++ src/index.js
@@ -7,34 +7,6 @@ const TaskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
-
-const multer = require('multer')
-// configuration
-const upload = multer({
-    dest: 'images',
-    limits: {
-        fileSize: 1000000,
-    },
-    // filters upload
-    fileFilter(req, file, callback) {
-        if (!file.originalname.match(/\.(doc|docx)$/)) {
-            return callback(new Error('Please upload a word document'))
-        }
-
-        callback(undefined, true)
-    }
-})
-
-// add upload as middleware
-app.post('/upload', upload.single('nameOfUpload'), (req, res) => {
-    res.send()
-}, (error, req, res, next) => {
-    // its important to provide 4 options above
-    res.status(400).send({
-        error: error.message
-    })
-})
-
 app.use(express.json())
 app.use(UserRouter)
 app.use(TaskRouter)
