c111eda7691f5502e57520a685382f28fdb9c94a
Joliver Estampador <joliverestampador@gmail.com>
2020-08-09 14:25:13 +0800

    more on supertest, wiping db before creating user'

--- test/user.test.js
+++ test/user.test.js
@@ -1,5 +1,19 @@
 const app = require('../src/app')
 const req = require('supertest')
+const User = require('../src/models/user')
+const { request } = require('express')
+
+const userOne = {
+    name: 'Mike',
+    email: 'mike@example.com',
+    password: 'Logmein+1234'
+}
+
+beforeEach(async () => {
+    // wipes out User document
+    await User.deleteMany()
+    await new User(userOne).save()
+})
 
 test('should sign up a new user', async () => {
     await req(app).post('/users').send({
@@ -7,4 +21,18 @@ test('should sign up a new user', async () => {
             email: 'andrew@example.com',
             password: 'Logmein+1234'
         }).expect(201)
+})
+
+test('Should login existing user', async () => {
+    await req(app).post('/users/login').send({
+        email: userOne.email,
+        password: userOne.password
+    }).expect(200)
+})
+
+test('Should not login non-existent user', async () => {
+    await req(app).post('/users/login').send({
+        email: 'notValid@gmail.com',
+        password: '12345'
+    }).expect(400)
 })
