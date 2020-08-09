998ebb2816a133495756c63dcb71e62ef66f8c59
Joliver Estampador <joliverestampador@gmail.com>
2020-08-09 11:44:17 +0800

    jest basics

--- test/math.test.js
+++ test/math.test.js
@@ -1,7 +1,26 @@
-test('Hello world', () => {
+const { 
+    calculateTip,
+    fahrenheitToCelsius,
+    celsiusToFahrenheit,
+} = require('../src/math')
 
-});
 
-test('this should fail', () => {
-    throw new Error('Error')
+test('should calculate tip', () => {
+    const total = calculateTip(10, .3)
+    expect(total).toBe(13)
+})
+
+test('should calculate default tip', () => {
+    const total = calculateTip(10)
+    expect(total).toBe(12)
+})
+
+test('converts fahrenheitToCelsius', () => {
+    const value = fahrenheitToCelsius(12)
+    expect(value).toBe( -11.11111111111111)
+})
+
+test('converts celsiusToFahrenheit', () => {
+    const value = celsiusToFahrenheit(145)
+    expect(value).toBe(293)
 })
