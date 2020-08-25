460adc35a0d30b9ae690d9bac3fd3abe0c9ec820
Joliver Estampador <joliverestampador@gmail.com>
2020-08-25 20:21:44 +0800

    Adding unit test for tasks

--- test/fixtures/db.js
+++ test/fixtures/db.js
@@ -4,11 +4,11 @@
 const mongoose = require('mongoose')
 const jwt = require('jsonwebtoken')
 const User = require('../../src/models/user')
+const Task = require('../../src/models/task')
 
 
  // simulate authentication
 const userOneId = new mongoose.Types.ObjectId
-
 const userOne = {
     _id: userOneId,
     name: 'Mike',
@@ -19,14 +19,52 @@ const userOne = {
     }]
 }
 
+const userTwoId = new mongoose.Types.ObjectId
+const userTwo = {
+    _id: userTwoId,
+    name: 'Popoy',
+    email: 'popy@example.com',
+    password: 'Logmein+1234',
+    tokens: [{
+        token: jwt.sign({ _id: userTwoId }, process.env.JWT_SECRET )
+    }]
+}
+
+const taskOne = {
+    _id: new mongoose.Types.ObjectId,
+    description: 'my first task from unit testing',
+    completed: false,
+    owner: userOneId,
+}
+
+const taskTwo = {
+    _id: new mongoose.Types.ObjectId,
+    description: 'my second task from unit testing',
+    completed: true,
+    owner: userOneId,
+}
+
+const taskThree = {
+    _id: new mongoose.Types.ObjectId,
+    description: 'my third task from unit testing',
+    completed: true,
+    owner: userTwoId,
+}
+
 const setupDatabase = async () => {
     // wipes out User document
     await User.deleteMany()
+    await Task.deleteMany()
     await new User(userOne).save()
+    await new User(userTwo).save()
+    await new Task(taskOne).save()
+    await new Task(taskTwo).save()
+    await new Task(taskThree).save()
 }
 
 module.exports = {
     userOneId,
     userOne,
     setupDatabase,
+    taskThree,
 }
--- test/task.test.js
+++ test/task.test.js
@@ -1,6 +1,41 @@
 const request = require('supertest')
+const app = require('../src/app')
 const Task = require('../src/models/task')
+const { userOneId, userOne, setupDatabase, taskThree } = require('./fixtures/db')
 
-test('should create task for user', () => {
-    
+beforeEach(setupDatabase)
+
+test('should create task for user', async () => {
+    const response = await request(app)
+        .post('/tasks')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send({
+            description: 'From my test'
+        })
+        .expect(201)
+        
+    const task = await Task.findById(response.body._id);
+    expect(task).not.toBeNull();
+    expect(task.completed).toBe(false);
+})
+
+test('should fetch tasks', async () => {
+    const response = await request(app)
+        .get('/tasks')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send()
+        .expect(200)
+
+    expect(response.body.length).toBe(2)
+})
+
+test('should be able to delete task', async () => {
+    const response = await request(app)
+        .delete(`/tasks/${taskThree._id}`)
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send()
+        .expect(404)
+
+    const task = await Task.findById(taskThree._id);
+    expect(task).not.toBeNull();
 })
